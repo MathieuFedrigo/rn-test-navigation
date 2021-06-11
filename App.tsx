151ee20c42f70729b313c5051ef0383223a18cf4
Mathieu Fedrigo <mathieuf@bam.tech>
2021-06-11 14:20:18 +0200

    Update Home to look like standard

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { HomeScreen } from './src/Home.component';
+import { HomePage } from './src/Home.component';
 
 const Stack = createStackNavigator();
 
@@ -9,7 +9,7 @@ const App: FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="HOME" component={HomePage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/Home.component.tsx
+++ src/Home.component.tsx
@@ -1,10 +1,15 @@
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import React, { FC } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
-export const HomeScreen: FC = () => {
+export const HomePage: FC = () => {
+  const { navigate } = useNavigation();
+  const goToProfile = () => navigate('PROFILE');
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>Home Page</Text>
+      <Button onPress={goToProfile} title="Go to profile" />
     </View>
   );
 };
